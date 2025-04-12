9d1b180e3b332fc4977514425e78b6803a878e61
Marjia Jaman <marjiajaman@Marjias-MacBook-Air.local>
2025-04-12 15:32:41 +0600

    added gitignore file

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const postsRoute = require("./routes/posts");
+app.use("/posts", postsRoute);
 
 module.express = app;
