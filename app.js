9af72e33acbd7b287968b2601ba5a832635dddff
Marjia Jaman <marjiajaman@Marjias-MacBook-Air.local>
2025-04-14 11:56:22 +0600

    Add image uploaded function and comment function

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const postsRoute = require("./routes/posts");
+const commentsRoute = require("./routes/comments");
+const userRoute = require("./routes/user");
+const imageRoute = require("./routes/images");
+
 const app = express();
 
-const postsRoute = require("./routes/posts");
-
 app.use(bodyParser.json());
+app.use("/uploads", express.static("uploads"));
 
 app.use("/posts", postsRoute);
+app.use("/comments", commentsRoute);
+app.use("/user", userRoute);
+app.use("/images", imageRoute);
 
 module.exports = app;
