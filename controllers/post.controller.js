fc93eccffd78cb8e8d12bc6c85c1e071980ea640
Marjia Jaman <marjiajaman@Marjias-MacBook-Air.local>
2025-04-21 20:13:28 +0600

    Use try and catch function in post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -4,52 +4,54 @@ const Post = models.Post;
 const Comment = models.Comment;
 const Category = models.Category;
 const User = models.User;
+const Image = models.Image;
 
-function save(req, res) {
-  const post = {
-    title: req.body.title,
-    content: req.body.content,
-    categoryId: req.body.category_id,
-    userId: req.userData.userId,
-  };
+async function save(req, res) {
+  try {
+    const post = {
+      title: req.body.title,
+      content: req.body.content,
+      categoryId: req.body.category_id,
+      userId: req.userData.userId,
+    };
 
-  const schema = {
-    title: { type: "string", optional: false, max: "100" },
-    content: { type: "string", optional: false, max: "500" },
-    categoryId: { type: "number", optional: false },
-  };
+    const schema = {
+      title: { type: "string", optional: false, max: "100" },
+      content: { type: "string", optional: false, max: "500" },
+      categoryId: { type: "number", optional: false },
+    };
 
-  const v = new Validator();
-  const validationResponse = v.validate(post, schema);
+    const v = new Validator();
+    const validationResponse = v.validate(post, schema);
 
-  if (validationResponse !== true) {
-    return res.status(400).json({
-      message: "Validation failed",
-      errors: validationResponse,
-    });
-  }
-
-  Category.findByPk(req.body.category_id).then((result) => {
-    if (result !== null) {
-      Post.create(post)
-        .then((result) => {
-          res.status(201).json({
-            message: "Post created successfully",
-            post: result,
-          });
-        })
-        .catch((error) => {
-          res.status(500).json({
-            message: "Something went wrong",
-            error: error,
-          });
-        });
-    } else {
-      res.status(400).json({
-        message: "Invalid Category ID",
+    if (validationResponse !== true) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: validationResponse,
       });
     }
-  });
+
+    const category = await Category.findByPk(req.body.category_id);
+
+    if (category) {
+      const createdPost = await Post.create(post);
+
+      if (createdPost) {
+        res.status(201).json({
+          message: "Post created successfully",
+          post: createdPost,
+        });
+      }
+    } else {
+      res.status(404).json({
+        message: "Category not found!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
+    });
+  }
 }
 
 async function show(req, res) {
@@ -66,7 +68,7 @@ async function show(req, res) {
           order: [["id", "DESC"]],
         },
         {
-          model: models.Image,
+          model: Image,
           order: [["id", "DESC"]],
         },
       ],
@@ -87,131 +89,154 @@ async function show(req, res) {
 }
 
 async function showComments(req, res) {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  const post = await Post.findByPk(id, {
-    include: [
-      {
-        model: Comment,
-        order: [["id", "DESC"]],
-      },
-    ],
-  });
+    const post = await Post.findByPk(id, {
+      include: [
+        {
+          model: Comment,
+          order: [["id", "DESC"]],
+        },
+      ],
+    });
 
-  if (post) {
-    res.status(200).json(post.Comments);
-  } else {
-    res.status(404).json({
-      message: "Post not found!",
+    if (post) {
+      res.status(200).json(post.Comments);
+    } else {
+      res.status(404).json({
+        message: "Post not found!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
     });
   }
 }
 
-function uploadImage(req, res) {
-  const id = req.params.id;
+async function uploadImage(req, res) {
+  try {
+    const id = req.params.id;
 
-  Post.findByPk(id).then((post) => {
-    if (post !== null) {
-      models.Image.create({
+    const post = await Post.findByPk(id);
+
+    if (post) {
+      const image = await Image.create({
         postId: post.id,
         imageUrl: "/uploads/" + req.file?.filename,
-      })
-        .then((result) => {
-          res.status(201).json({
-            message: "Image Uploaded!",
-            image: result,
-          });
-        })
-        .catch((error) => {
-          res.status(500).json({
-            message: "Something went wrong!",
-          });
+      });
+
+      if (image) {
+        res.status(201).json({
+          message: "Image Uploaded!",
+          image: image,
         });
+      }
     } else {
-      res.status(500).json({
+      res.status(400).json({
         message: "Invalid Request!",
       });
     }
-  });
-}
-
-function index(req, res) {
-  Post.findAll()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Something went wrong!",
-      });
-    });
-}
-
-function update(req, res) {
-  const id = req.params.id;
-  const updatedPost = {
-    title: req.body.title,
-    content: req.body.content,
-    categoryId: req.body.category_id,
-  };
-
-  const userId = req.userData.userId;
-
-  const schema = {
-    title: { type: "string", optional: false, max: "100" },
-    content: { type: "string", optional: false, max: "500" },
-    categoryId: { type: "number", optional: false },
-  };
-
-  const v = new Validator();
-  const validationResponse = v.validate(updatedPost, schema);
-
-  if (validationResponse !== true) {
-    return res.status(400).json({
-      message: "Validation failed",
-      errors: validationResponse,
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
     });
   }
-
-  Category.findByPk(req.body.category_id).then((result) => {
-    if (result !== null) {
-      Post.update(updatedPost, { where: { id: id, userId: userId } })
-        .then((result) => {
-          res.status(200).json({
-            message: "Post updated successfully",
-            post: updatedPost,
-          });
-        })
-        .catch((error) => {
-          res.status(500).json({
-            message: "Something went wrong!",
-            error: error,
-          });
-        });
-    } else {
-      res.status(400).json({
-        message: "Invalid Category ID",
-      });
-    }
-  });
 }
 
-function destroy(req, res) {
-  const id = req.params.id;
-  const userId = req.userData.userId;
-
-  Post.destroy({ where: { id: id, userId: userId } })
-    .then((result) => {
-      res.status(200).json({
-        message: "Post deleted successfully",
+async function index(req, res) {
+  try {
+    const getAllPost = await Post.findAll();
+    if (getAllPost) {
+      res.status(200).json(getAllPost);
+    } else {
+      res.status(400).json({
+        message: "Invalid request!",
       });
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Something went wrong!",
-        error: error,
-      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
     });
+  }
+}
+
+async function update(req, res) {
+  try {
+    const id = req.params.id;
+    const updatedPost = {
+      title: req.body.title,
+      content: req.body.content,
+      categoryId: req.body.category_id,
+    };
+
+    const userId = req.userData.userId;
+
+    const schema = {
+      title: { type: "string", optional: false, max: "100" },
+      content: { type: "string", optional: false, max: "500" },
+      categoryId: { type: "number", optional: false },
+    };
+
+    const v = new Validator();
+    const validationResponse = v.validate(updatedPost, schema);
+
+    if (validationResponse !== true) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: validationResponse,
+      });
+    }
+
+    const category = await Category.findByPk(req.body.category_id);
+
+    if (category) {
+      const post = await Post.update(updatedPost, {
+        where: { id: id, userId: userId },
+      });
+
+      if (post) {
+        res.status(200).json({
+          message: "Post updated successfully",
+          post: updatedPost,
+        });
+      } else {
+        res.status(400).json({
+          message: "Category not found!",
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
+      error: error,
+    });
+  }
+}
+
+async function destroy(req, res) {
+  try {
+    const id = req.params.id;
+
+    const post = await Post.findByPk(id);
+    if (post) {
+      const destroyPost = await post.destroy();
+      if (destroyPost) {
+        res.status(200).json({
+          message: "Post deleted successfully",
+        });
+      }
+    } else {
+      res.status(400).json({
+        message: "Invalid request!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
+    });
+  }
 }
 
 module.exports = {
