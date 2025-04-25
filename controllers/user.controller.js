4c5303fe8d6a3075824aa736be53d4f9ba219707
Marjia Jaman <marjiajaman@Marjias-MacBook-Air.local>
2025-04-25 18:06:37 +0600

    Use try and catch function in user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,107 +3,114 @@ const User = models.User;
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-function signUp(req, res) {
-  User.findOne({ where: { email: req.body.email } })
-    .then((result) => {
-      if (result) {
-        res.status(409).json({
-          message: "Email already exists!",
-        });
-      } else {
-        bcryptjs.genSalt(10, function (err, salt) {
-          bcryptjs.hash(req.body.password, salt, function (err, hash) {
-            const user = {
-              name: req.body.name,
-              email: req.body.email,
-              password: hash,
-            };
-
-            User.create(user)
-              .then((user) => {
-                const token = jwt.sign(
-                  {
-                    email: user.email,
-                    userId: user.id,
-                  },
-                  process.env.JWT_KEY,
-                  function (err, token) {
-                    res.status(200).json({
-                      message: "Authentication successful!",
-                      token: token,
-                    });
-                  }
-                );
-              })
-              .catch((error) => {
-                res.status(500).json({
-                  message: "Something went wrong!",
-                });
-              });
-          });
-        });
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Something went wrong!",
+async function signUp(req, res) {
+  try {
+    const findUser = await User.findOne({ where: { email: req.body.email } });
+    if (findUser) {
+      res.status(409).json({
+        message: "Email already exists!",
       });
-    });
-}
+    } else {
+      const salting = await bcryptjs.genSalt(10);
+      const hashing = await bcryptjs.hash(req.body.password, salting);
 
-function login(req, res) {
-  User.findOne({ where: { email: req.body.email } })
-    .then((user) => {
-      if (user === null) {
-        res.status(401).json({
-          message: "Invalid credentials!",
-        });
-      } else {
-        bcryptjs.compare(
-          req.body.password,
-          user.password,
-          function (err, result) {
-            if (result) {
-              const token = jwt.sign(
-                {
-                  email: user.email,
-                  userId: user.id,
-                },
-                process.env.JWT_KEY,
-                function (err, token) {
-                  res.status(200).json({
-                    message: "Authentication successful!",
-                    token: token,
-                  });
-                }
-              );
-            } else {
-              res.status(401).json({
-                message: "Incorrect password!",
-              });
-            }
+      const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashing,
+      };
+
+      const createUser = await User.create(user);
+      if (createUser) {
+        const token = jwt.sign(
+          {
+            email: user.email,
+            userId: user.id,
+          },
+          process.env.JWT_KEY,
+          function (err, token) {
+            res.status(200).json({
+              message: "Authentication successful!",
+              token: token,
+            });
           }
         );
+      } else {
+        res.status(500).json({
+          message: "Something went wrong!",
+        });
       }
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Something went wrong!",
-      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
     });
+  }
 }
 
-function updateProfileImg(req, res) {
-  User.findByPk(req.userData.userId).then((user) => {
-    User.update(
-      { profileImageUrl: "/uploads/" + req.file?.filename },
-      { where: { id: user.id } }
-    ).then((result) => {
-      res.status(201).json({
-        message: "Image Updated!",
-      });
+async function login(req, res) {
+  try {
+    const findingUser = await User.findOne({
+      where: { email: req.body.email },
     });
-  });
+    if (findingUser === null) {
+      res.status(401).json({
+        message: "Invalid credentials!",
+      });
+    } else {
+      bcryptjs.compare(
+        req.body.password,
+        findingUser.password,
+        function (err, result) {
+          if (result) {
+            const token = jwt.sign(
+              {
+                email: findingUser.email,
+                userId: findingUser.id,
+              },
+              process.env.JWT_KEY,
+              function (err, token) {
+                res.status(200).json({
+                  message: "Authentication successful!",
+                  token: token,
+                });
+              }
+            );
+          } else {
+            res.status(401).json({
+              message: "Incorrect password!",
+            });
+          }
+        }
+      );
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
+    });
+  }
+}
+
+async function updateProfileImg(req, res) {
+  try {
+    const user = await User.findByPk(req.userData.userId);
+
+    if (user) {
+      const updateUserPic = await User.update(
+        { profileImageUrl: "/uploads/" + req.file?.filename },
+        { where: { id: user.id } }
+      );
+      if (updateUserPic) {
+        res.status(201).json({
+          message: "Image Updated!",
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
+    });
+  }
 }
 
 module.exports = {
